56ea8f71bf788b2c618124c82c515f439d08fcb3
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-18 09:21:36 +0100

    Updates

---
+++ precache-manifest.88cefed5b3827f5ebeefd5e7be84a5f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51952cad5c9cddc98b6db46b301e5ce",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2ccad06651a329c8ee3e",
+    "url": "/monster-rolodex/static/css/main.a93a0ffb.chunk.css"
+  },
+  {
+    "revision": "a209dc162028312e21ac",
+    "url": "/monster-rolodex/static/js/2.a24f9c2a.chunk.js"
+  },
+  {
+    "revision": "2ccad06651a329c8ee3e",
+    "url": "/monster-rolodex/static/js/main.5323e7dd.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
